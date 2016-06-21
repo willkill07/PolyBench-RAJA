5327c8366646fbdf7f19317d2c94794639466d10
Will Killian <william.killian@gmail.com>
2016-06-20 22:13:52 -0700

    [updated] fdtd-2d floyd-warshall gemm gemver

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -8,48 +8,47 @@
 /* Include benchmark-specific header. */
 #include "gemm.hpp"
 
-
 static void init_array(int ni,
                        int nj,
                        int nk,
                        double *alpha,
                        double *beta,
-                       double C[NI][NJ],
-                       double A[NI][NK],
-                       double B[NK][NJ]) {
+                       Arr2D<double>* C,
+                       Arr2D<double>* A,
+                       Arr2D<double>* B) {
   *alpha = 1.5;
   *beta = 1.2;
   RAJA::forallN<Independent2D> (
     RAJA::RangeSegment { 0, ni },
     RAJA::RangeSegment { 0, nj },
     [=] (int i, int j) {
-      C[i][j] = (double)((i * j + 1) % ni) / ni;
+      C->at(i,j) = (double)((i * j + 1) % ni) / ni;
     }
   );
   RAJA::forallN<Independent2D> (
     RAJA::RangeSegment { 0, ni },
     RAJA::RangeSegment { 0, nk },
     [=] (int i, int j) {
-      A[i][j] = (double)(i * (j + 1) % nk) / nk;
+      A->at(i,j) = (double)(i * (j + 1) % nk) / nk;
     }
   );
   RAJA::forallN<Independent2D> (
     RAJA::RangeSegment { 0, nk },
     RAJA::RangeSegment { 0, nj },
     [=] (int i, int j) {
-      B[i][j] = (double)(i * (j + 2) % nj) / nj;
+      B->at(i,j) = (double)(i * (j + 2) % nj) / nj;
     }
   );
 }
 
-static void print_array(int ni, int nj, double C[NI][NJ]) {
+static void print_array(int ni, int nj, const Arr2D<double>* C) {
   int i, j;
   fprintf(stderr, "==BEGIN DUMP_ARRAYS==\n");
   fprintf(stderr, "begin dump: %s", "C");
   for (i = 0; i < ni; i++)
     for (j = 0; j < nj; j++) {
       if ((i * ni + j) % 20 == 0) fprintf(stderr, "\n");
-      fprintf(stderr, "%0.2lf ", C[i][j]);
+      fprintf(stderr, "%0.2lf ", C->at(i,j));
     }
   fprintf(stderr, "\nend   dump: %s\n", "C");
   fprintf(stderr, "==END   DUMP_ARRAYS==\n");
@@ -60,13 +59,13 @@ static void kernel_gemm(int ni,
                         int nk,
                         double alpha,
                         double beta,
-                        double C[NI][NJ],
-                        double A[NI][NK],
-                        double B[NK][NJ]) {
+                        Arr2D<double>* C,
+                        const Arr2D<double>* A,
+                        const Arr2D<double>* B) {
 #pragma scop
   RAJA::forall<RAJA::omp_parallel_for_exec> (0, ni, [=] (int i) {
     RAJA::forall<RAJA::simd_exec> (0, nj, [=] (int j) {
-      C[i][j] *= beta;
+      C->at(i,j) *= beta;
     });
     RAJA::forallN<
       RAJA::NestedPolicy<
@@ -76,7 +75,7 @@ static void kernel_gemm(int ni,
     >(RAJA::RangeSegment { 0, nk },
       RAJA::RangeSegment { 0, nj },
       [=] (int k, int j) {
-        C[i][j] += alpha * A[i][k] * B[k][j];
+        C->at(i,j) += alpha * A->at(i,k) * B->at(k,j);
       });
   });
 #pragma endscop
@@ -88,20 +87,14 @@ int main(int argc, char **argv) {
   int nk = NK;
   double alpha;
   double beta;
-  double(*C)[NI][NJ];
-  C = (double(*)[NI][NJ])polybench_alloc_data((NI) * (NJ), sizeof(double));
-  double(*A)[NI][NK];
-  A = (double(*)[NI][NK])polybench_alloc_data((NI) * (NK), sizeof(double));
-  double(*B)[NK][NJ];
-  B = (double(*)[NK][NJ])polybench_alloc_data((NK) * (NJ), sizeof(double));
-  init_array(ni, nj, nk, &alpha, &beta, *C, *A, *B);
-  polybench_timer_start();
-  kernel_gemm(ni, nj, nk, alpha, beta, *C, *A, *B);
-  polybench_timer_stop();
-  polybench_timer_print();
-  if (argc > 42 && !strcmp(argv[0], "")) print_array(ni, nj, *C);
-  free((void *)C);
-  free((void *)A);
-  free((void *)B);
+	Arr2D<double> C { ni, nj }, A { ni, nk }, B { nk, nj };
+
+  init_array(ni, nj, nk, &alpha, &beta, &C, &A, &B);
+  {
+		util::block_timer { "GEMM" };
+		kernel_gemm(ni, nj, nk, alpha, beta, &C, &A, &B);
+	}
+	if (argc > 42)
+		print_array(ni, nj, &C);
   return 0;
 }
--- src/gemver.cpp
+++ src/gemver.cpp
@@ -8,44 +8,43 @@
 /* Include benchmark-specific header. */
 #include "gemver.hpp"
 
-
 static void init_array(int n,
                        double *alpha,
                        double *beta,
-                       double A[N][N],
-                       double u1[N],
-                       double v1[N],
-                       double u2[N],
-                       double v2[N],
-                       double w[N],
-                       double x[N],
-                       double y[N],
-                       double z[N]) {
+                       Arr2D<double>* A,
+                       Arr1D<double>* u1,
+                       Arr1D<double>* v1,
+                       Arr1D<double>* u2,
+                       Arr1D<double>* v2,
+                       Arr1D<double>* w,
+                       Arr1D<double>* x,
+                       Arr1D<double>* y,
+                       Arr1D<double>* z) {
   *alpha = 1.5;
   *beta = 1.2;
   double fn = (double)n;
   RAJA::forall<RAJA::omp_parallel_for_exec> (0, n, [=] (int i) {
-    u1[i] = i;
-    u2[i] = ((i + 1) / fn) / 2.0;
-    v1[i] = ((i + 1) / fn) / 4.0;
-    v2[i] = ((i + 1) / fn) / 6.0;
-    y[i] = ((i + 1) / fn) / 8.0;
-    z[i] = ((i + 1) / fn) / 9.0;
-    x[i] = 0.0;
-    w[i] = 0.0;
+    u1->at(i) = i;
+    u2->at(i) = ((i + 1) / fn) / 2.0;
+    v1->at(i) = ((i + 1) / fn) / 4.0;
+    v2->at(i) = ((i + 1) / fn) / 6.0;
+    y->at(i) = ((i + 1) / fn) / 8.0;
+    z->at(i) = ((i + 1) / fn) / 9.0;
+    x->at(i) = 0.0;
+    w->at(i) = 0.0;
     RAJA::forall<RAJA::simd_exec> (0, n, [=] (int j) {
-      A[i][j] = (double)(i * j % n) / n;
+      A->at(i,j) = (double)(i * j % n) / n;
     });
   });
 }
 
-static void print_array(int n, double w[N]) {
+static void print_array(int n, const Arr1D<double>* w) {
   int i;
   fprintf(stderr, "==BEGIN DUMP_ARRAYS==\n");
   fprintf(stderr, "begin dump: %s", "w");
   for (i = 0; i < n; i++) {
     if (i % 20 == 0) fprintf(stderr, "\n");
-    fprintf(stderr, "%0.2lf ", w[i]);
+    fprintf(stderr, "%0.2lf ", w->at(i));
   }
   fprintf(stderr, "\nend   dump: %s\n", "w");
   fprintf(stderr, "==END   DUMP_ARRAYS==\n");
@@ -54,38 +53,38 @@ static void print_array(int n, double w[N]) {
 static void kernel_gemver(int n,
                           double alpha,
                           double beta,
-                          double A[N][N],
-                          double u1[N],
-                          double v1[N],
-                          double u2[N],
-                          double v2[N],
-                          double w[N],
-                          double x[N],
-                          double y[N],
-                          double z[N]) {
+                          Arr2D<double>* A,
+                          const Arr1D<double>* u1,
+                          const Arr1D<double>* v1,
+                          const Arr1D<double>* u2,
+                          const Arr1D<double>* v2,
+                          Arr1D<double>* w,
+                          Arr1D<double>* x,
+                          const Arr1D<double>* y,
+                          const Arr1D<double>* z) {
 #pragma scop
   RAJA::forallN<Independent2DTiled> (
     RAJA::RangeSegment { 0, n },
     RAJA::RangeSegment { 0, n },
     [=] (int i, int j) {
-      A[i][j] = A[i][j] + u1[i] * v1[j] + u2[i] * v2[j];
+      A->at(i,j) = A->at(i,j) + u1->at(i) * v1->at(j) + u2->at(i) * v2->at(j);
     }
   );
   RAJA::forallN<RAJA::NestedPolicy<RAJA::ExecList<RAJA::omp_parallel_for_exec,RAJA::simd_exec>>> (
     RAJA::RangeSegment { 0, n },
     RAJA::RangeSegment { 0, n },
     [=] (int i, int j) {
-      x[i] = x[i] + beta * A[j][i] * y[j];
+      x->at(i) = x->at(i) + beta * A->at(j,i) * y->at(j);
     }
   );
   RAJA::forall<RAJA::omp_parallel_for_exec> (0, n, [=] (int i) {
-    x[i] = x[i] + z[i];
+    x->at(i) = x->at(i) + z->at(i);
   });
   RAJA::forallN<RAJA::NestedPolicy<RAJA::ExecList<RAJA::omp_parallel_for_exec,RAJA::simd_exec>>> (
     RAJA::RangeSegment { 0, n },
     RAJA::RangeSegment { 0, n },
     [=] (int i, int j) {
-      w[i] = w[i] + alpha * A[i][j] * x[j];
+      w->at(i) = w->at(i) + alpha * A->at(i,j) * x->at(j);
     }
   );
 #pragma endscop
@@ -95,38 +94,18 @@ int main(int argc, char **argv) {
   int n = N;
   double alpha;
   double beta;
-  double(*A)[N][N];
-  A = (double(*)[N][N])polybench_alloc_data((N) * (N), sizeof(double));
-  double(*u1)[N];
-  u1 = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*v1)[N];
-  v1 = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*u2)[N];
-  u2 = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*v2)[N];
-  v2 = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*w)[N];
-  w = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*x)[N];
-  x = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*y)[N];
-  y = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  double(*z)[N];
-  z = (double(*)[N])polybench_alloc_data(N, sizeof(double));
-  init_array(n, &alpha, &beta, *A, *u1, *v1, *u2, *v2, *w, *x, *y, *z);
-  polybench_timer_start();
-  kernel_gemver(n, alpha, beta, *A, *u1, *v1, *u2, *v2, *w, *x, *y, *z);
-  polybench_timer_stop();
-  polybench_timer_print();
-  if (argc > 42 && !strcmp(argv[0], "")) print_array(n, *w);
-  free((void *)A);
-  free((void *)u1);
-  free((void *)v1);
-  free((void *)u2);
-  free((void *)v2);
-  free((void *)w);
-  free((void *)x);
-  free((void *)y);
-  free((void *)z);
+	Arr2D<double> A { n, n };
+	Arr1D<double> u1 { n }, w { n };
+	Arr1D<double> v1 { n }, x { n };
+	Arr1D<double> u2 { n }, y { n };
+	Arr1D<double> v2 { n }, z { n };
+
+	init_array(n, &alpha, &beta, &A, &u1, &v1, &u2, &v2, &w, &x, &y, &z);
+	{
+		util::block_timer t { "GEMVER" };
+		kernel_gemver(n, alpha, beta, &A, &u1, &v1, &u2, &v2, &w, &x, &y, &z);
+  }
+  if (argc > 42)
+		print_array(n, &w);
   return 0;
 }
